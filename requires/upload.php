141236b392b72564579f2bcadda672a7c81db591
ItIsJR <86535087+ItIsJR@users.noreply.github.com>
2021-06-27 14:21:54 -0400

    Update upload.php

--- requires/upload.php
+++ requires/upload.php
@@ -1,4 +1,5 @@
 <?php
+start_session();
 
 if(isset($_POST['submit'])){
     $file = $_FILES['file'];
@@ -12,7 +13,7 @@ if(isset($_POST['submit'])){
     $fileExt = explode('.', $fileName);
     $fileActualExt = strtolower(end($fileExt));
     
-    $allowed = array('mp3','mp4', 'png', 'jpg', 'jpeg', 'html', 'css', 'js', 'pdf', 'go', 'php', 'mysql');
+    $allowed = array('mp3','mp4', 'png', 'jpg', 'jpeg', 'html', 'css', 'js', 'pdf', 'go', 'php', 'mysql', 'svg');
     
     if(in_array($fileActualExt, $allowed)){
         if ($fileError === 0){
@@ -31,4 +32,14 @@ if(isset($_POST['submit'])){
         echo "you cant upload files of this type";
     }
 }
+
+$sql = "UPDATE users SET diskused='4' WHERE id=1";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
 ?>
