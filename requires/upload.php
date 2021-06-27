24c49c8c318ae18be38e959af93f4cf25ef3e549
ItIsJR <86535087+ItIsJR@users.noreply.github.com>
2021-06-27 16:24:04 -0400

    Added the disk space counter

--- requires/upload.php
+++ requires/upload.php
@@ -1,6 +1,16 @@
 <?php
-start_session();
-
+   session_start();
+   
+   require_once "./requires/config.php";
+   $str = "SELECT diskspace, diskused, diskrem FROM users WHERE id = '".$_SESSION['id']."'";
+   $r = mysqli_query($link, $str);
+   
+   while($row = mysqli_fetch_array($r)) {
+       $diskspace = $row['diskspace'];
+       $diskused = $row['diskused'];
+       $diskrem = $row['diskrem'];
+   }
+   
 if(isset($_POST['submit'])){
     $file = $_FILES['file'];
     
@@ -13,15 +23,14 @@ if(isset($_POST['submit'])){
     $fileExt = explode('.', $fileName);
     $fileActualExt = strtolower(end($fileExt));
     
-    $allowed = array('mp3','mp4', 'png', 'jpg', 'jpeg', 'html', 'css', 'js', 'pdf', 'go', 'php', 'mysql', 'svg');
+    $allowed = array('mp3','mp4', 'png', 'jpg', 'jpeg', 'html', 'css', 'js', 'pdf', 'go', 'php', 'mysql', 'svg', 'txt', 'md');
     
     if(in_array($fileActualExt, $allowed)){
         if ($fileError === 0){
-            if ($fileSize < 9999999999999999) {
+            if ($fileSize < 1500000) {
                 $fileNameNew = uniqid('', true).".".$fileActualExt;
                 $fileDestination = 'uploads/'.$fileNameNew;
                 move_uploaded_file($fileTmpName, $fileDestination);
-                header("Location: https://sharevideos.ga/uploads/".$fileNameNew);
             } else {
                 echo "your file is too big";
             }
@@ -33,13 +42,22 @@ if(isset($_POST['submit'])){
     }
 }
 
-$sql = "UPDATE users SET diskused='4' WHERE id=1";
+$sql = "";
 
-if ($conn->query($sql) === TRUE) {
-  echo "Record updated successfully";
-} else {
-  echo "Error updating record: " . $conn->error;
+if ($diskused == 0)
+{
+    $sql = "UPDATE users SET diskused='$fileSize' WHERE id = 1";
+}elseif ($diskused > 0) 
+{
+  $disknow = $diskused + $fileSize;
+  $sql = "UPDATE users SET diskused='$disknow' WHERE id = 1";
 }
 
-$conn->close();
+if ($link->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $link->error;
+}
+
+$link->close();
 ?>
